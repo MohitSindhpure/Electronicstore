bca9ad03cdea4a5f04a140f665ad698acbc911ca
MohitSindhpure <mohitsindhpure1998@gmail.com>
2023-04-19 19:59:48 +0530

    usercontroller testcase add init method 1

--- src/test/java/com/shruteekatech/electronicstore/controllertest/UserControllerTest.java
+++ src/test/java/com/shruteekatech/electronicstore/controllertest/UserControllerTest.java
@@ -1,19 +1,31 @@
 package com.shruteekatech.electronicstore.controllertest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shruteekatech.electronicstore.BaseTest;
 import com.shruteekatech.electronicstore.controller.UserController;
 import com.shruteekatech.electronicstore.dtos.UserDto;
 import com.shruteekatech.electronicstore.model.User;
 import com.shruteekatech.electronicstore.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-public class UserControllerTest {
+public class UserControllerTest extends BaseTest {
 
     @MockBean
     private UserService userService;
@@ -34,26 +46,52 @@ public class UserControllerTest {
     public void init()
     {
         user= User.builder()
-                .name("Isha")
+                .name("Mohit")
                 .about("Software Enginner")
-                .email("isha@gmail.com").gender("Female")
-                .imageName("abc.png").password("Isha@12234").build();
+                .email("Mohit@gmail.com").gender("Male")
+                .imageName("abc.png").password("Mohit@12234").build();
         user1= User.builder()
-                .name("Ashu")
+                .name("Rohan")
                 .about("Software Enginner")
-                .email("Ashu@gmil.com").gender("Female")
+                .email("Rohan@gmil.com").gender("male")
                 .imageName("abc.png").password("1234@12").build();
         user2= User.builder()
-                .name("Amruta")
+                .name("Shubham")
                 .about("Software Enginner")
-                .email("Amruta@gmil.com").gender("Female")
+                .email("Shubham@gmil.com").gender("male")
                 .imageName("abc.png").password("1234@12").build();
-        userDto = UserDto.builder().about("My name").name("Isha Rathore")
+        userDto = UserDto.builder().about("My name").name("Mohit Sindhpure")
                 .imageName("xyz.png")
                 .email("abc@gmail.com")
-                .password("123@3isa").build();
+                .password("123@3mohit").build();
+
+    }
+
+    @Test
+    public void createUserTest() throws Exception {
+
+
+        UserDto dto = modelMapper.map(user, UserDto.class);
+        Mockito.when(userService.createUser(Mockito.any())).thenReturn(dto);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/Api/Users/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(converobjectTojsonString(user))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect((ResultMatcher) jsonPath("$.email").exists());
 
 
     }
 
+    private String converobjectTojsonString(User user) {
+        try
+        {
+            return new ObjectMapper().writeValueAsString(user);
+        } catch (RuntimeException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
