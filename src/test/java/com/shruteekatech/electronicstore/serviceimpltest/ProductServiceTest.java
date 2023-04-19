8f2cb48df1d165db4459940af61bbc1a5a0c8ae5
MohitSindhpure <mohitsindhpure1998@gmail.com>
2023-04-19 18:47:28 +0530

    productService TestCase add list product

--- src/test/java/com/shruteekatech/electronicstore/serviceimpltest/ProductServiceTest.java
+++ src/test/java/com/shruteekatech/electronicstore/serviceimpltest/ProductServiceTest.java
@@ -1,10 +1,14 @@
 package com.shruteekatech.electronicstore.serviceimpltest;
 
+import com.shruteekatech.electronicstore.dtos.ProductDto;
+import com.shruteekatech.electronicstore.model.Product;
 import com.shruteekatech.electronicstore.repository.ProductRepo;
 import com.shruteekatech.electronicstore.service.ProductService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 public class ProductServiceTest {
 
@@ -17,4 +21,9 @@ public class ProductServiceTest {
     @Autowired
     private ModelMapper modelMapper;
 
+    Product product1,product2,product3;
+
+    List<Product> products;
+
+    ProductDto productDto;
 }
