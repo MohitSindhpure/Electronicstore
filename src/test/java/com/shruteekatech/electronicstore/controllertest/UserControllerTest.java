2865ff569399eed639201cc48cd4f718df4e0b4a
MohitSindhpure <mohitsindhpure1998@gmail.com>
2023-04-19 19:46:59 +0530

    usercontroller testcase add init method

--- src/test/java/com/shruteekatech/electronicstore/controllertest/UserControllerTest.java
+++ src/test/java/com/shruteekatech/electronicstore/controllertest/UserControllerTest.java
@@ -1,7 +1,10 @@
 package com.shruteekatech.electronicstore.controllertest;
 
 import com.shruteekatech.electronicstore.controller.UserController;
+import com.shruteekatech.electronicstore.dtos.UserDto;
+import com.shruteekatech.electronicstore.model.User;
 import com.shruteekatech.electronicstore.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +26,34 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    User user,user1,user2;
+
+    UserDto userDto;
+    @BeforeEach
+    public void init()
+    {
+        user= User.builder()
+                .name("Isha")
+                .about("Software Enginner")
+                .email("isha@gmail.com").gender("Female")
+                .imageName("abc.png").password("Isha@12234").build();
+        user1= User.builder()
+                .name("Ashu")
+                .about("Software Enginner")
+                .email("Ashu@gmil.com").gender("Female")
+                .imageName("abc.png").password("1234@12").build();
+        user2= User.builder()
+                .name("Amruta")
+                .about("Software Enginner")
+                .email("Amruta@gmil.com").gender("Female")
+                .imageName("abc.png").password("1234@12").build();
+        userDto = UserDto.builder().about("My name").name("Isha Rathore")
+                .imageName("xyz.png")
+                .email("abc@gmail.com")
+                .password("123@3isa").build();
+
+
+    }
 
 }
