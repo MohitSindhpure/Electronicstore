3b4edefd3d8261a4022199b43b4defb8cef17fd6
MohitSindhpure <mohitsindhpure1998@gmail.com>
2023-04-20 16:15:06 +0530

    CategoryService TestCase add category object

--- src/test/java/com/shruteekatech/electronicstore/serviceimpltest/CategoryServiceTest.java
+++ src/test/java/com/shruteekatech/electronicstore/serviceimpltest/CategoryServiceTest.java
@@ -1,11 +1,15 @@
 package com.shruteekatech.electronicstore.serviceimpltest;
 
 import com.shruteekatech.electronicstore.BaseTest;
+import com.shruteekatech.electronicstore.dtos.CategoryDto;
 import com.shruteekatech.electronicstore.repository.CategoryRepo;
 import com.shruteekatech.electronicstore.service.CategoryService;
+import jdk.jfr.Category;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 public class CategoryServiceTest extends BaseTest {
 
@@ -17,4 +21,12 @@ public class CategoryServiceTest extends BaseTest {
 
     @Autowired
     private ModelMapper modelMapper;
+
+
+    Category category1, category2, category3;
+
+    List<Category> categories;
+
+    CategoryDto categoryDto;
+
 }
